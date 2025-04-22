c6d421c0cf238d3346cb1fc1f2c5c26cf5f05750
Ctibor Šebák <sebakctibor@gmail.com>
2025-04-22 09:34:22 +0200

    Initial Commit

---
+++ SwiftyBooks/SwiftyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyBooksApp.swift
+//  SwiftyBooks
+//
+//  Created by Ctibor Šebák on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyBooksTests/SwiftyBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyBooksTests.swift
+//  SwiftyBooksTests
+//
+//  Created by Ctibor Šebák on 22.04.2025.
+//
+
+import Testing
+@testable import SwiftyBooks
+
+struct SwiftyBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
